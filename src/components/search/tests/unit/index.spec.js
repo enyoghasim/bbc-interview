dee1a96e3a73dde79f7f0d3007917282391f6cf8
GIFT! <geezyenyoghasim@gmail.com>
2021-03-06 15:59:50 +0100

    test : made test for input component

--- src/components/search/tests/unit/index.spec.js
+++ src/components/search/tests/unit/index.spec.js
@@ -14,9 +14,17 @@ describe("SEARCHBOX", () => {
     expect(wrapper.props("placeHolderText")).toBe("SEARCH YOUR CITY!");
     expect(wrapper.props("inputType")).toBe("search");
   });
-  test("CHECKING IF CHANGE ON INPUT EMITS A CUSTOM EVENT", async () => {
+  test("CHECKING IF CHANGE ON INPUT REFLECTS AT ALL", async () => {
     const input = wrapper.find("input[type='search']");
     await input.setValue("some value");
     expect(input.element.value).toBe("some value");
+  });
+  test("CHECKING IF CHANGE ON INPUT EMITS AN EVENT WITH REQUIRED DETAILS", async () => {
+    const input = wrapper.find("input[type='search']");
+    await input.setValue("some value");
+    expect(wrapper.emitted().inputed).toBeTruthy();
+    const eventObject = wrapper.emitted().inputed[0][0];
+    expect(eventObject.newValue).toEqual("some value");
+    expect(eventObject.nameOfInput).toEqual("search");
   });
 });
