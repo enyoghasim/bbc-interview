aa1f37c33272165d22c17bc68c52b73fbc72119a
Ofobrukweta jeff <oghenerukevwejeff@gmail.com>
2021-03-12 14:33:34 +0100

    chore: added the webpack config optimization files

---
+++ src/components/footer/tests/unit/index.spec.js
@@ -0,0 +1,16 @@
+import { shallowMount } from "@vue/test-utils";
+import Footer from "../../index.vue";
+
+
+const Wrapper = shallowMount(<Footer/>,{
+  propsData:{
+
+  }
+})
+
+
+describe("Footer Component", ()=>{
+  it("footer show correctly in dom",()=>{
+    expect(Wrapper).toBe(Wrapper)
+  });
+});
